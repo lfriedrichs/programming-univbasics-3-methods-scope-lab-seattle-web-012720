8638a704e149ffca712ac207a074c3e257b57284
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-12 22:43:47 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
-phrase = "It's-a me, Mario!"
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$phrase = "It's-a me, Mario!"
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
-  puts phrase
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
